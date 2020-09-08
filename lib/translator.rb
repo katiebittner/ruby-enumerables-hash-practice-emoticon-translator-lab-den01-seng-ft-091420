43a5f2d10f1b07972993b46041b900ccfbcf6571
Katie B <katiebittner1@gmail.com>
2020-09-08 21:41:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,10 +14,13 @@ def load_library(file_path)
   emoticon_dictionary
 end
 
-def get_japanese_emoticon(file_path)
+def get_japanese_emoticon(file_path, western_emoticon)
   emoticon_dictionary = load_library(file_path)
-  
-  
+  emoticon_dictionary.each do |language, emoji|
+    
+    if emoji[0] == western_emoticon
+    emoji[1]
+    
   
 end
 
