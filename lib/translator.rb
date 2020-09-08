5671c0f5fcf85400ee84fafa9adeda14c5c6c109
Katie B <katiebittner1@gmail.com>
2020-09-08 21:54:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,6 +33,7 @@ def get_english_meaning(file_path, japanese_emoticon)
   emoticon_dictionary.each do |emotion, emoticons|
     if emoticons[:japanese] == japanese_emoticon
       english_meaning = emotion
+    end
   end
   english_meaning
 end
