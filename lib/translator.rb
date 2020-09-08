12e66cc063185f1e39df6e1ca98d23fced1a2b34
Katie B <katiebittner1@gmail.com>
2020-09-08 21:27:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,9 @@ require "yaml"
 emoticons = YAML.load_file('emoticons.yml')
 
 def load_library
+  emoticon_dictionary = {}
   emoticons.each do |emotion, emoji|
-    puts 
+    emoticon_dictionary 
   end
 end
 
@@ -16,10 +17,15 @@ def get_english_meaning
   # code goes here
 end
 
-
-
 def list_dwarves(array)
   array.each_with_index do |dwarf, index|
     puts "#{index+1}. #{dwarf}"
   end
 end
+
+   {
+      'happy' => {
+         :english => ":)",
+         :japanese => "(＾ｖ＾)"
+      }
+   }
